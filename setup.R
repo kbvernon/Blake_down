621fa6931ea6adf2ae93aa1db1de29148a17991f
Kenneth Blake Vernon <kenneth.b.vernon@gmail.com>
2022-02-13 13:03:27 -0700

    mostly stylistic changes

--- setup.R
+++ setup.R
@@ -1,16 +1,34 @@
 
+### LIBRARIES ###
+library(bibtex)
+library(dplyr)
 library(glue)
+library(googlesheets4)
 library(here)
 library(icons)
-library(jsonlite)
 library(kableExtra)
 library(knitr)
 library(RefManageR)
-library(tidyverse)
+library(tidyr)
 
-knitr::opts_chunk$set(echo = FALSE, 
-                      warning = FALSE, 
-                      message = FALSE)
+### SETTINGS ###
+knitr::opts_chunk$set(
+  echo = FALSE, 
+  warning = FALSE, 
+  message = FALSE,
+  results = "asis"
+)
 
-options(knitr.kable.NA = "",
-        kableExtra.html.bsTable = TRUE)
+options(knitr.kable.NA = "")
+
+### HELPER FUNCTIONS ###
+fa <- icons::fontawesome # version 5
+ai <- icons::academicons
+
+source("cv_lists.R")
+
+### CREDENTIALS ###
+load(here("_credentials.RData"))
+
+### BIBLIOGRAPHY ###
+bib <- cv_get_bibliography(zotero, collection)
